b38542d7d67354ad8ae8006c5c126c259c873d6f
Nicolas Landier <nicolas@landier.org>
2012-10-19 20:44:39 +0200

    Deleted files

--- crawler/Person.py
+++
@@ -1,6 +0,0 @@
-class Person:
-    def __init__(self, firstName = None, lastName = None, gender = None):
-        self.FirstName = firstName
-        self.LastName = lastName
-        self.Gender = gender
-        self.BirthDate = None
